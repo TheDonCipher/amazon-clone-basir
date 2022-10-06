20568925d1190c82bcf60a4cabb0ed942efbb2dc
TheDonCipher <eugeniusratshipa@gmail.com>
2022-10-06 22:19:53 +0200

    loaded products from sanity

---
+++ components/ProductItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import NextLink from 'next/link';
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material';
+import { urlForThumbnail } from '../utils/image';
+
+export const ProductItem = ({ product }) => {
+  return (
+    <Card>
+      <NextLink href={`/product/${product.slug.current}`} passHref>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={urlForThumbnail(product.image)}
+            title={product.name}
+          ></CardMedia>
+          <CardContent>
+            <Typography>{product.name}</Typography>
+            <Typography>
+              {product.rating} ({product.numReviews} reviews)
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </NextLink>
+      <CardActions>
+        <Typography>${product.price}</Typography>
+        <Button size="small" color="primary">
+          Add to cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
---
+++ utils/config.js
@@ -0,0 +1,6 @@
+const config = {
+  projectId: 'j8klrp4s', //change on deployment
+  dataset: 'production',
+};
+
+export default config;
