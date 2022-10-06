9009a4bf84bb167bd443f410f049904d0bb69bd0
TheDonCipher <eugeniusratshipa@gmail.com>
2022-10-06 19:31:46 +0200

    home page made

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
 import Head from 'next/head';
 import { Typography } from '@mui/material';
+import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Lucello and Shuminski</title>
-        <meta name="description" content="Art e-commerce website" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Typography component="h1" variant="h1">
-        Lucello and Shuminski
-      </Typography>
-    </div>
-  );
+  return <Layout>List Products</Layout>;
 }
