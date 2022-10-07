974fb3a94096927f9b162da9f7c1f60aef822996
TheDonCipher <eugeniusratshipa@gmail.com>
2022-10-07 09:45:26 +0200

    darkmode switch added

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ import {
   Container,
   CssBaseline,
   Link,
+  Switch,
   ThemeProvider,
   Toolbar,
   Typography,
@@ -12,8 +13,14 @@ import {
 import Head from 'next/head';
 import NextLink from 'next/link';
 import classes from '../utils/classes';
+import { useContext } from 'react';
+import { Store } from '../utils/store';
+import jsCookie from 'js-cookie';
 
 export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
+
   const theme = createTheme({
     components: {
       MuiLink: {
@@ -35,7 +42,7 @@ export default function Layout({ title, description, children }) {
       },
     },
     palette: {
-      mode: 'light',
+      mode: darkMode ? 'dark' : 'light',
       primary: {
         main: '#f0c000',
       },
@@ -45,6 +52,11 @@ export default function Layout({ title, description, children }) {
     },
   });
 
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? 'DARK_MODE_OFF' : 'DARK_MODE_ON' });
+    const newDarkMode = !darkMode;
+    jsCookie.set('darkMode', newDarkMode ? 'ON' : 'OFF');
+  };
   return (
     <>
       <Head>
@@ -57,13 +69,21 @@ export default function Layout({ title, description, children }) {
         <CssBaseline />
         <AppBar position="static" sx={classes.appbar}>
           <Toolbar sx={classes.toolbar}>
-            <NextLink href="/" passHref>
-              <Link>
-                <Typography sx={classes.brand}>
-                  Lucello and Shuminski
-                </Typography>
-              </Link>
-            </NextLink>
+            <Box display="flex" alignItems="center">
+              <NextLink href="/" passHref>
+                <Link>
+                  <Typography sx={classes.brand}>
+                    Lucello and Shuminski
+                  </Typography>
+                </Link>
+              </NextLink>
+            </Box>
+            <Box>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+            </Box>
           </Toolbar>
         </AppBar>
         <Container component="main" sx={classes.main}>
